3c99f600ded96d5fdbce4e43f11e95f212fb0773
Darren Burns <darrenb900@gmail.com>
2024-12-07 18:59:47 +0000

    Day 7 part 1

---
+++ aoc24/day7.py
@@ -0,0 +1,36 @@
+from files import get_resource_path
+
+
+def day7_part1() -> int:
+    data = get_resource_path("day7.txt").read_text().splitlines()
+    total = 0
+    for line in data:
+        target, numbers = line.split(":")
+        target = int(target.strip())
+        numbers = [int(number.strip()) for number in numbers.split()]
+
+        def hits_target(index: int, prefix: int) -> bool:
+            # For each value, consider both + and * with all the possible prefixes
+            # that came before it (recursively generated).
+            if index == len(numbers) - 1:
+                return (
+                    prefix + numbers[index] == target
+                    or prefix * numbers[index] == target
+                )
+            plus_hit = hits_target(index + 1, prefix + numbers[index])
+            times_hit = hits_target(index + 1, prefix * numbers[index])
+            return plus_hit or times_hit
+
+        if hits_target(0, 0):
+            total += target
+
+    return total
+
+
+def day7_part2() -> int:
+    pass
+
+
+if __name__ == "__main__":
+    print(day7_part1())
+    print(day7_part2())
